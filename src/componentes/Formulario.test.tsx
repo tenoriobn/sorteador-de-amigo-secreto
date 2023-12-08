ac92cffbdfbcce9907289ceab30d3f1c4ecc0dd1
Bruno Tenório <tenoriobn@gmail.com>
2023-12-08 11:03:04 -0300

    Fazendo o teste passar

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
+import Formulario from './Formulario';
 
 test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
 
---
+++ src/componentes/Formulario.tsx
@@ -0,0 +1,8 @@
+export default function Formulario() {
+  return (
+    <form action="">
+      <input type="text" placeholder="Insira os nomes dos participantes" />
+      <button disabled={true}>Adicionar</button>
+    </form>
+  )
+}
