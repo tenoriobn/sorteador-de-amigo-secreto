bef58a6d04780ab6890db837bc6584c985aa95af
Bruno Tenório <tenoriobn@gmail.com>
2023-12-11 18:11:41 -0300

    Evoluindo o formulário

---
+++ src/state/hooks/useAdicionarParticipante.ts
@@ -0,0 +1,10 @@
+import { useSetRecoilState } from "recoil";
+import { listaParticipantesState } from "../atom";
+
+export const useAdicionarParticipante = () => {
+  const setLista = useSetRecoilState(listaParticipantesState);
+
+  return (nomeDoParticipante: string) => {
+    return setLista(listaAtual => [...listaAtual, nomeDoParticipante]);
+  };
+};
