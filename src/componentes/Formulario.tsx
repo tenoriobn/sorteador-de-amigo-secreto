6212636e347f3e7de2ba1465796c96e001f0dade
Bruno Tenório <tenoriobn@gmail.com>
2023-12-12 10:20:20 -0300

    useRef e o foco no input

--- src/componentes/Formulario.tsx
+++ src/componentes/Formulario.tsx
@@ -1,8 +1,32 @@
+import { useRef, useState } from "react"
+import { useAdicionarParticipante } from "../state/hooks/useAdicionarParticipante";
+
 export default function Formulario() {
+  const [nome, setNome] = useState('');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const adicionarNaLista = useAdicionarParticipante();
+
+  const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+
+    adicionarNaLista(nome)
+
+    setNome('');
+    inputRef.current?.focus();
+  }
+
   return (
-    <form action="">
-      <input type="text" placeholder="Insira os nomes dos participantes" />
-      <button disabled={true}>Adicionar</button>
+    <form onSubmit={adicionarParticipante}>
+      <input 
+        type="text" 
+        placeholder="Insira os nomes dos participantes" 
+        ref={inputRef}
+        value={nome}
+        onChange={evento => setNome(evento.target.value)}
+      />
+      <button disabled={!nome}>Adicionar</button>
     </form>
   )
 }
