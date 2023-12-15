777405c3ab0161dd79b9426800651734c679e09c
Bruno Tenório <tenoriobn@gmail.com>
2023-12-15 15:53:09 -0300

    Fotografando componentes

---
+++ src/paginas/Configuracao.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Formulario from '../componentes/Formulario';
+import ListaParticipantes from '../componentes/ListaParticipantes';
+import Rodape from '../componentes/Rodape';
+
+const Configuracao = () => {
+  return (
+    <>
+      <Formulario />
+      <ListaParticipantes />
+      <Rodape />
+    </>
+  )
+}
+
+export default Configuracao;
