c2a8216c81230eef37db11dbbdb3bcc5a3f4d037
Bruno Tenório <tenoriobn@gmail.com>
2023-12-18 19:23:12 -0300

    Extraindo a regra de negócio

---
+++ src/state/helpers/realizarSorteio.test.ts
@@ -0,0 +1,13 @@
+import { realizarSorteio } from "./realizarSorteio";
+
+describe('dado um sorteio de amigo secreto', () => {
+  test('cada participante não sorteie o próprio nome', () => {
+    const participantes = ['Ana', 'Catarina', 'Jualiana', 'João', 'Vinicios', 'Nathália',];
+
+    const sorteio = realizarSorteio(participantes)
+    participantes.forEach(participante => {
+      const amigoSecreto = sorteio.get(participante)
+      expect(amigoSecreto).not.toEqual(participante)
+    })
+  })
+})
