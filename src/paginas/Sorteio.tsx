6261215224e0b806e326e10f0f45177c8c4fc935
Bruno Tenório <tenoriobn@gmail.com>
2023-12-18 18:17:58 -0300

    Página de sorteio

---
+++ src/paginas/Sorteio.tsx
@@ -0,0 +1,17 @@
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+
+const Sorteio = () => {
+  const participantes = useListaDeParticipantes();
+  
+  return (
+    <section>
+      <form action="">
+        <select name="participanteDaVez" id="participanteDaVez">
+          {participantes.map(participante => <option key={participante}>{participante}</option>)}
+        </select>
+      </form>
+    </section>
+  )
+}
+
+export default Sorteio;
